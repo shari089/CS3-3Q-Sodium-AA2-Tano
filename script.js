a6187f9514608b8bfdb3d4d9c9c90cf00d3253a2
shari089 <sybtano22@src.pshs.edu.ph>
2025-02-13 11:47:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function finalPopulation() {
+	let initialPopulation = prompt("Input intial population: ");
+	let rateOfGrowth = prompt("Input rate of growth: ");
+	let timeInHours = prompt("Input time in hours: ");
+	
+	let floatInitialPopulation = parseFloat(initialPopulation);
+	let floatRateOfGrowth = parseFloat(rateOfGrowth);
+	let floatTimeInHours = parseFloat(timeInHours);
+	
+	let finalPopulation = Math.round(floatInitialPopulation * Math.pow(Math.E, floatRateOfGrowth * floatTimeInHours));
+			
+	let regionOfMonster = prompt("Input the region of the monster: ");
+	let nameOfMonster = prompt("Input the name of the monster: ");
+	let monster = (regionOfMonster.concat(' ').concat(nameOfMonster)).toUpperCase();
+	
+	document.getElementById('result').innerHTML = ('After' + ' ' + floatTimeInHours + ' ' + 'hours' + ' ' + 'the population of' + ' ' + monster + ' ' + 'has risen to' + ' ' + finalPopulation + '!');
+			
+}
